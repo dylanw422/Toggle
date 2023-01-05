6fa6493cc7eb7039d82c39040511b2a06367959a
dylanw422 <71285357+dylanw422@users.noreply.github.com>
2023-01-05 09:37:13 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,62 @@
-let circle = document.querySelector('.circle')
 let toggle = document.querySelector('#toggle')
-let switcharea = document.querySelector('#switch-area')
+let switchArea = document.querySelector('#switcharea')
+let loaderOne = document.querySelector('#loader1')
+let firstSlider = document.querySelector('#slider1')
+let secondSlider = document.querySelector('#slider2')
+let squares = document.querySelector('.square')
+let sqOne = document.querySelector('#one')
+let sqTwo = document.querySelector('#two')
+let sqThree = document.querySelector('#three')
+let sqFour = document.querySelector('#four')
+let sqFive = document.querySelector('#five')
+let sqSix = document.querySelector('#six')
+let sqSeven = document.querySelector('#seven')
+let sqEight = document.querySelector('#eight')
+let sqNine = document.querySelector('#nine')
+let sqTen = document.querySelector('#ten')
 
-let switchValue = 'off'
+let toggleValue = 'off'
 
-function toggleSwitchToGreen() {
-    switcharea.style.animation = 'colorChange 0.5s'
-    switcharea.style.animationFillMode = 'forwards'
-    circle.style.animation = 'slide 0.5s'
-    circle.style.animationFillMode = 'forwards'
-    switchValue = 'on'
-}
+toggle.addEventListener('click', () => {
+    console.log('clicked')
+    if (toggleValue === 'off' ) {
+        toggle.style.animation = 'toggleOn 1s'
+        toggle.style.animationFillMode = 'forwards'
+        toggleValue = 'on'
 
-function toggleSwitchToRed() {
-    switcharea.style.animation = 'colorChangeReverse 0.5s'
-    switcharea.style.animationFillMode = 'forwards'
-    circle.style.animation = 'slideBackwards 0.5s'
-    circle.style.animationFillMode = 'forwards'
-    switchValue = 'off'
-}
+        loaderOne.style.animation = 'loaderPosition 5s infinite'
+        firstSlider.style.animation = 'loading 5s infinite'
+        secondSlider.style.animation = 'sliderTwo 3s linear infinite'
 
+        sqOne.style.animation = 'btOne 2s linear infinite'       
+        sqTwo.style.animation = 'btTwo 2s linear infinite'
+        sqThree.style.animation = 'btThree 2s linear infinite'
+        sqFour.style.animation = 'btFour 2s linear infinite'
+        sqFive.style.animation = 'btFive 2s linear infinite'
+        sqSix.style.animation = 'btSix 2s linear infinite'
+        sqSeven.style.animation = 'btSeven 2s linear infinite'
+        sqEight.style.animation = 'btEight 2s linear infinite'
+        sqNine.style.animation = 'btNine 2s linear infinite'
+        sqTen.style.animation = 'btTen 2s linear infinite'
 
-switcharea.addEventListener('click', () => {
-    if (switchValue === 'off') {
-        toggleSwitchToGreen()
-        console.log(switchValue)
-    } else if (switchValue === 'on') {
-        toggleSwitchToRed()
-        console.log(switchValue)
+    } else if (toggleValue === 'on') {
+        toggle.style.animation = 'toggleOff 1s'
+        toggle.style.animationFillMode = 'forwards'
+        toggleValue = 'off'
+
+        loaderOne.style.animation = ''
+        firstSlider.style.animation = ''
+        secondSlider.style.animation = ''
+
+        sqOne.style.animation = ''
+        sqTwo.style.animation = ''
+        sqThree.style.animation = ''
+        sqFour.style.animation = ''
+        sqFive.style.animation = ''
+        sqSix.style.animation = ''
+        sqSeven.style.animation = ''
+        sqEight.style.animation = ''
+        sqNine.style.animation = ''
+        sqTen.style.animation = ''
     }
 })
-
-
